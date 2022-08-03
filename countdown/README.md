d1082f208bcc5d86e0ca381e4fd699c44fc7a1d5
Matheus <Matheussandi@hotmail.com>
2022-08-03 18:34:33 -0300

    Funcionamento do cronometro

--- countdown/README.md
+++ countdown/README.md
@@ -1,15 +1,39 @@
 <p align="center">
-  <img src="https://user-images.githubusercontent.com/29473781/182260754-e4cb3e25-6a91-4f1d-90cc-496046992b97.gif" />
+  <img src="https://user-images.githubusercontent.com/29473781/182712095-6719b5b3-403f-42d0-87c3-b631ee118848.gif" />
 </p>
 
 # :computer: Projeto
-Site para contagem regressiva de algum evento, com um botão de cadastro fictício. Desenvolvido através do [layout](https://www.figma.com/file/Qo8Bb9amqSlyVhp5Gfc80V/DD-%2F-Countdown-(Copy)?node-id=20%3A230) da Rocketseat.
+Site para contagem regressiva de algum evento específico, com botão de cadastro fictício. Desenvolvido através do [layout](https://www.figma.com/file/Qo8Bb9amqSlyVhp5Gfc80V/DD-%2F-Countdown-(Copy)?node-id=20%3A230) da Rocketseat.
+
+# :pushpin: Recursos
+
+- [x] Contagem regressiva
+
+- [x] Inscrição fictícia
 
 
-# :technologist: Tecnologias
+# :man_technologist: Tecnologias
  
 - [ReactJS](https://pt-br.reactjs.org/)
 
 - [Typescript](https://www.typescriptlang.org/)
 
 - [Tailwind](https://tailwindcss.com/)
+
+
+# :calendar: Editar data do evento
+
+Caso queira escolher outra data para o evento basta mudar a váriavel dateEvent.
+
+```js
+const dateEvent = new Date("15/08/2022 23:59:59")
+```
+# :arrow_forward: Instação e execucução
+
+```bash
+# Instala as depêndias
+yarn
+
+# Executa aplicação
+yarn start
+```
--- countdown/src/App.tsx
+++ countdown/src/App.tsx
@@ -1,9 +1,194 @@
-import { Body } from "./components/Body";
+import { useEffect, useState } from 'react';
+
+import bottomImage from './assets/bottom-image.svg';
+import animationData from './assets/animation.json';
+import topImage from './assets/top-image.svg';
+import rocket from './assets/rocket.svg';
+
+import Lottie from 'react-lottie';
+import Modal from 'react-modal';
+
+Modal.setAppElement("#root");
 
 export default function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [eventTime, setEventTime] = useState(false);
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  useEffect(() => {
+    const dateEvent = new Date("15/08/2022 23:59:59");
+
+    const interval = setInterval(() => {
+      const now = new Date();
+
+      // comando getTime() converte a data para milissegundos
+      const difference = dateEvent.getTime() - now.getTime();
+
+      const second = 1000;
+      const minute = second * 60;
+      const hour = minute * 60;
+      const day = hour * 24;
+
+      const d = Math.floor(difference / day)
+      const h = Math.floor((difference % day) / hour);
+      const m = Math.floor((difference % hour) / minute);
+      const s = Math.floor((difference % minute) / second);
+
+      setDays(d);
+      setHours(h);
+      setMinutes(m);
+      setSeconds(s);
+
+      if (d <= 0 && h <= 0 && m <= 0 && s <= 0) {
+        setEventTime(true);
+        return 0;
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
-      <Body />
+      <img
+        className='w-full'
+        src={topImage}
+        alt="topImage"
+      />
+
+      <div className='grid lg:grid-cols-2'>
+        <div className='text-center mt-14'>
+          <h1 className="text-purple font-bold text-4xl">Pronto para lançar em...</h1>
+
+          <div className='text-light-grey font-light mt-5 mx-5 space-x-14 '>
+            <span>Dias</span>
+            <span>Horas</span>
+            <span>Minutos</span>
+            <span>Segundos</span>
+          </div>
+
+          <div className='text-black text-7xl font-normal'>
+            {
+              eventTime ? (
+                <>
+                  <span>00:00:00:00</span>
+                  <div className='fixed inset-0 flex justify-center items-center pointer-events-none'>
+                    <Lottie options={defaultOptions} />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <span>
+                    {String(days).padStart(2, '0')}:
+                    {String(hours).padStart(2, '0')}:
+                    {String(minutes).padStart(2, '0')}:
+                    {String(seconds).padStart(2, '0')}
+                  </span>
+                </>
+              )
+            }
+          </div>
+
+          <p className='mt-5'>Inscreva-se para saber mais sobre o lançamento</p>
+
+          {
+            eventTime ? (
+              <>
+                <button
+                  className='bg-purple opacity-60 text-white rounded-lg px-8 py-2.5 mt-8 cursor-default'
+                >
+                  Inscreva-se
+                </button>
+              </>
+            ) : (
+              <>
+                <button
+                  onClick={openModal}
+                  className='bg-purple text-white rounded-lg px-8 py-2.5 mt-8'
+                >
+                  Inscreva-se
+                </button>
+              </>
+            )
+          }
+
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex justify-center items-center"
+          >
+            <div>
+              <div className="bg-white p-2 rounded">
+                <div className="text-right p-1 mr-6">
+                  <button
+                    onClick={closeModal}
+                    className="bg-purple inline-flex justify-center py-0.5 px-2 rounded text-white fixed">X</button>
+                </div>
+
+
+                <form action="">
+                  <h1 className="text-center">Formulario</h1>
+
+                  <div className='p-2'>
+                    <label className="mr-2">Nome:</label>
+                    <input
+                      className='w-9/12 p-1'
+                      type="text"
+                      required />
+                  </div>
+
+                  <div className='p-2'>
+                    <label className="mr-3">Email:</label>
+                    <input
+                      className='w-9/12 p-1'
+                      type="email"
+                      required />
+                  </div>
+
+                  <div className='flex justify-center'>
+                    <button type="submit" className='bg-purple p-2 rounded text-white mt-2'>
+                      Enviar
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+
+          </Modal>
+        </div>
+
+        <div className='text-center mt-8 lg:mt-0'>
+          <img
+            className='inline'
+            src={rocket} alt="rocket" />
+        </div>
+      </div>
+
+      <img
+        className=' w-full bottom-0 fixed h-36'
+        src={bottomImage}
+        alt="topImage"
+      />
     </>
-  );
+  )
 }
